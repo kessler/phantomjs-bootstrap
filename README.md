caf55bc90ffea0fd3b4adb45f31582daf289013c
Yaniv Kessler <yanivk@gmail.com>
2012-10-28 20:39:03 +0200

    initial

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 phantomjs-bootstrap
 ===================
+
+A simple bootstrap script to load PhantomJS with additional dependencies. Clone or download bootstrap.js and run. (lib directory and main.js are just an example)
+
+Syntax:
+
+	phantomjs bootstrap.js <main script> <additional scripts>
+
+
+Additional information
+----------------------
+
+*All the loaded resources must be relative to the directory from whence you start phantomjs.
+*The main script will be evaludated after the main script, and the additional scripts will be evaluated in the order they were specified
+*The additional scripts will a ***;*** separated list of either directories or files. In case of a directory the bootstrap will iterate and evaluate all the .js child files that reside inside it.
---
+++ bootstrap.js
@@ -0,0 +1,54 @@
+var fs = require('fs');
+
+if (phantom.args.length === 0) 
+ 	throw 'usage:\n phantomjs bootstrap.js <main script to run> <additional libs separated by;>';
+
+var main = fs.workingDirectory + fs.separator + phantom.args[0];
+
+if (!fs.exists(main)) 
+	throw 'cannot find ' + main;
+	
+loadLibraries();
+
+readAndEvaluateScript(main);
+
+function loadLibraries() {
+	if (phantom.args.length > 1) {
+		
+		var libPath = phantom.args[1].split(';');
+
+		for (var i = 0; i < libPath.length; i++) {
+			var currentPath = fs.workingDirectory + fs.separator + libPath[i];
+
+			if (!fs.exists(currentPath)) 
+				throw 'cannot find ' + currentPath;
+
+			if (fs.isDirectory(currentPath)) {
+				var files = fs.list(currentPath);
+				
+				for (var x = 0; x < files.length; x++) {
+				
+					var filename = files[x];
+					var extension = filename.substr(-3);
+
+					if (typeof(extension) === 'string') {
+						extension = extension.toLowerCase();
+
+						if (extension === '.js')					
+							readAndEvaluateScript(currentPath + fs.separator + filename);
+					}
+				}
+
+			} else {
+
+				readAndEvaluateScript(currentPath);
+			}
+		}	
+	}
+}
+
+function readAndEvaluateScript(path) {
+	console.log('loading ' + path);
+	var script = fs.read(path);
+	eval(script);
+}
